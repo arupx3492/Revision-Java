82fb852f645e5044e7717a5b8cbe4d0d19d127bd
Arup Mahato <arup71062@gmail.com>
2023-03-01 11:07:51 +0530

    PrintAddress

---
+++ Java introduction/PrintAddress/src/com/masai/Main.java
@@ -0,0 +1,10 @@
+package com.masai;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This is My First Java Application");
+		System.out.println("===================================");
+		System.out.println("My Name is : Arup Mahato");
+		System.out.println("My Address is : Jargaria, Kurarbaid, Boro. Purulia,723131");
+	}
+}
